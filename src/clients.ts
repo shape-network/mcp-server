df62582a66a74bc7e1aab2f2205c0f068ab503f2
William H <william.hermozo@protonmail.com>
2025-07-17 16:14:28 +0200

    add clients config

---
+++ src/clients.ts
@@ -0,0 +1,17 @@
+import { createPublicClient, http } from 'viem';
+import { shape, shapeSepolia } from 'viem/chains';
+import { config } from './config';
+import { Alchemy, Network } from 'alchemy-sdk';
+
+export const alchemyClient = new Alchemy({
+  apiKey: config.alchemyApiKey,
+  network: config.isMainnet ? Network.SHAPE_MAINNET : Network.SHAPE_SEPOLIA,
+});
+
+export const publicClient = createPublicClient({
+  chain: config.isMainnet ? shape : shapeSepolia,
+  transport: http(),
+  batch: {
+    multicall: true,
+  },
+});
