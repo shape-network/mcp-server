cfa6fd4e32d22bcc949b4a5651c3ff183818c43e
William H <william.hermozo@protonmail.com>
2025-07-17 16:00:37 +0200

    add addresses

---
+++ src/addresses.ts
@@ -0,0 +1,8 @@
+import { shape, shapeSepolia } from 'viem/chains';
+
+export const addresses = {
+  gasback: {
+    [shape.id]: '0xf5e602c87d675E978F097503aedE4A766285a08B',
+    [shapeSepolia.id]: '0xdF329d59bC797907703F7c198dDA2d770fC45034',
+  },
+};
