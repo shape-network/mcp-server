1626784ece4b5859c2169ea6bb3cd1a91c1d4696
William H <william.hermozo@protonmail.com>
2025-07-21 17:10:18 +0200

    clients

--- src/clients.ts
+++ src/clients.ts
@@ -1,17 +1,26 @@
+import { Alchemy, Network } from 'alchemy-sdk';
 import { createPublicClient, http } from 'viem';
 import { shape, shapeSepolia } from 'viem/chains';
 import { config } from './config';
-import { Alchemy, Network } from 'alchemy-sdk';
 
-export const alchemyClient = new Alchemy({
+export const alchemy = new Alchemy({
   apiKey: config.alchemyApiKey,
-  network: config.isMainnet ? Network.SHAPE_MAINNET : Network.SHAPE_SEPOLIA,
+  network:
+    config.chainId === shape.id ? Network.SHAPE_MAINNET : Network.SHAPE_SEPOLIA,
 });
 
-export const publicClient = createPublicClient({
-  chain: config.isMainnet ? shape : shapeSepolia,
-  transport: http(),
-  batch: {
-    multicall: true,
-  },
-});
+export function rpcClient() {
+  const chainId = config.chainId;
+  const chain = chainId === shape.id ? shape : shapeSepolia;
+  const rootUrl = chainId === shape.id ? 'shape-mainnet' : 'shape-sepolia';
+
+  return createPublicClient({
+    chain,
+    transport: http(
+      `https://${rootUrl}.g.alchemy.com/v2/${config.alchemyApiKey}`
+    ),
+    batch: {
+      multicall: true,
+    },
+  });
+}
