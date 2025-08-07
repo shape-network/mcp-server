5d8a17a5f46f7b92350cd0e3fec751e429402013
William H <william.hermozo@protonmail.com>
2025-08-07 13:03:30 +0200

    rarible keys

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,7 @@ import { shape } from 'viem/chains';
 export const config = {
   chainId: Number(process.env.CHAIN_ID),
   alchemyApiKey: process.env.ALCHEMY_API_KEY as string,
+  raribleApiKey: process.env.RARIBLE_API_KEY as string,
   isMainnet: Number(process.env.CHAIN_ID) === shape.id,
   redisUrl: process.env.REDIS_URL as string,
   defaultRpcUrl:
